270231910c45a60aba45a475865b66c1df81bad1
Philip On <nakurunet@gmail.com>
2016-12-22 13:34:43 +0300

    Commenting

--- car_class.py
+++ car_class.py
@@ -1,8 +1,6 @@
 class Car(object):
 	"""Class car.  Car can be trailer or saloon. Car should have 4 doors except Porsche or Koenigsegg.Car should have 4 wheels except trailer. Car name and model should be instance property of Car. Car model default should be GM. Car name default should beGeneral. """
-	#name = "General"
-	#model = "GM"
-	#v_type = None
+
 	def __init__(self, name="General",model="GM", v_type='' ):
 		self.name = name
 		self.model = model
@@ -12,14 +10,14 @@ class Car(object):
 		self.num_of_doors = 4 if not(name =='Porshe' or name == 'Koenigsegg') else 2
 		self.num_of_wheels = 8 if v_type == "trailer" else 4
 		
-	
-	def is_saloon(self):# Check car type
+	# Check the type of car method
+	def is_saloon(self):
 		if self.v_type != "trailer":
 			self.v_type = "saloon"
 			return True #True if is saloon
 		return False
-	
-	def drive(self, moving_speed):# Check move speed 
+	# Function to Check the moving speed of vehicle
+	def drive(self, moving_speed):
 		if moving_speed == 3:
 			self.speed = 1000
 		elif moving_speed == 7:
